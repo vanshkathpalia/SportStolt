b895efecd6ee3d5adc72a4450333d9a8b1c15ace
Vansh Kumar <vanshkathpalia@gmail.com>
2024-11-15 12:48:16 +0530

    to check the fix with git email

--- apps/frontend/src/pages/Posts.tsx
+++ apps/frontend/src/pages/Posts.tsx
@@ -1,5 +1,5 @@
 export const Posts = () => {
     return <div>
-        all the posts
+        all the posts, will hit the /bulk from backend and also this page will have the new post publish link...
     </div>
 }
