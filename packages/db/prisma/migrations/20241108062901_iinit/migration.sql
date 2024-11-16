cc4b3023b1db3f6974970f75f680fca6e799617f
Vansh Kumar <vanshkathpalia@gmail.com>
2024-11-16 23:02:44 +0530

    nothing comming from the backend for post, wrong backend call but the frontend is proper

--- packages/db/prisma/migrations/20241108062901_iinit/migration.sql
+++ packages/db/prisma/migrations/20241108062901_iinit/migration.sql
@@ -1,13 +1,13 @@
 -- CreateTable
-CREATE TABLE "Blog" (
+CREATE TABLE "Post" (
     "id" SERIAL NOT NULL,
     "authorId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "published" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "Blog" ADD CONSTRAINT "Blog_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
